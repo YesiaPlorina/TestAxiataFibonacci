103c0845d1daa7e860cc4163c9a23be04607c12e
yesia <yesia.plorina@pgi-data.com>
2021-09-07 19:06:59 +0700

    first init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import React, { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState(null);
+  const [table, setTable] = useState([]);
+  const [fib, setFib] = useState([]);
+  const [show, setShow] = useState([]);
+
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  };
+  const onClikInput = () => {
+    if (input) {
+      var myArr = [];
+      for (var i = 0; i < input * input; i++) {
+        if (i < 2) {
+          myArr.push(i);
+        } else {
+          myArr.push(myArr[i - 2] + myArr[i - 1]);
+        }
+      }
+      setFib(myArr);
+
+      let fibo = [];
+      let tampung = [];
+      for (let j = 0; j < myArr.length; j++) {
+        const elementFib = myArr[j];
+        console.log(j % input, elementFib);
+
+        if ((j + 1) % input === 0) {
+          tampung.push(elementFib);
+          fibo.push(tampung);
+          tampung = [];
+        } else if (j === fib.length - 1) {
+          fibo.push(tampung);
+        } else {
+          tampung.push(elementFib);
+        }
+      }
+      console.log(fibo, "Hasil fib");
+      setShow(fibo);
+
+      setInput("");
+    } else {
+      setShow([]);
+      alert("Please input number of table");
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            marginBottom: "50px",
+          }}
         >
-          Learn React
-        </a>
+          <input
+            class="text-input"
+            type="number"
+            value={input}
+            name="input"
+            onChange={handleInput}
+            placeholder="input number"
+          />
+
+          <div className="btn-create-data" onClick={onClikInput}>
+            <label className="label-sm-bold" style={{ cursor: "pointer" }}>
+              input
+            </label>
+          </div>
+        </div>
+        <div>
+          <table>
+            <tbody className="tbody">
+              {show &&
+                show.map((item, i) => (
+                  <tr key={i}>
+                    {item.map((item1, j) =>
+                      item === 0 ? (
+                        <td key={j} align="left"></td>
+                      ) : (
+                        <td key={j} align="left">
+                          {item1}
+                        </td>
+                      )
+                    )}
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
       </header>
     </div>
   );
